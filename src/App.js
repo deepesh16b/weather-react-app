fe02f5f7814376ed27f1d539046a6b7ed0acab5d
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-03-31 05:27:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import Restaurant from "./components/basics/Restaurant";
 // import UseState from "./components/hooks/UseState";
-import UseEffect from "./components/hooks/UseEffect";
+// import UseEffect from "./components/hooks/UseEffect";
 // import UseReducer from "./components/hooks/UseReducer";
 // import Todo from "./components/todoReact/todo";
 // import './components/todoReact/style.css';
